0534c8dd37180df093c7a94cb62db00e6f8fafa9
karansapra124 <sapra7112@gmail.com>
2025-03-01 14:34:40 +0530

    Improved Contact Section

--- src/Components/Home/Contact.jsx
+++ src/Components/Home/Contact.jsx
@@ -6,29 +6,31 @@ const Contact = () => {
   return (
     <>
       <Container>
-        <div className="flex gap-4 max-w-screen-lg mx-auto">
-          <div className="flex flex-col w-1/2 shadow-md shadow-black bg-[#d1361b] text-white p-6 rounded-md">
+        <div className="flex gap-4 max-w-screen-xl  mx-auto">
+          <div className="flex flex-col  w-1/2 shadow-md shadow-black bg-[#d1361b] text-white p-6 rounded-md">
             <h1 className="text-2xl font-bold mb-2 ">Get In Touch</h1>
-            <div className="font-semibold ">
-              <h2 className="text-lg text-center ">Phone No</h2>
-              <p className="mb-4 text-center">9990499116, 9990075511</p>
-              <h2 className="text-lg text-center">Main Office:</h2>
-              <p className="mb-4 text-center">
+            <div className="font-semibold flex flex-col px-2 bg-white text-gray-900 rounded-md justify-center items-center h-full">
+              <h2 className="text-lg text-center  text-primary-color font-bold">Phone No:</h2>
+              <div className="h-1 w-10 mx-auto my-1.5 bg-primary-color rounded-full"></div>
+              <p className="mb-4 text-center font-medium">9990499116, 9990075511</p>
+              <h2 className="text-lg text-center text-primary-color font-bold">Main Office:</h2>
+              <div className="h-1 w-10 mx-auto my-1.5 bg-primary-color rounded-full"></div>
+              <p className="mb-4 text-center font-medium">
                 H. NO. 652, Ground floor, Front portion SHAHBAD Mohammadpur, New
                 Delhi -110061
               </p>
-              <h2 className="text-lg text-center">BRANCH ADDRESS:</h2>
-              <p className="text-center">
+              <h2 className="text-lg text-center text-primary-color font-bold">BRANCH ADDRESS:</h2>
+              <div className="h-1 w-10 mx-auto my-1.5 bg-primary-color rounded-full"></div>
+              <p className="text-center font-medium">
                 Ground floor, Meharam nagar, Opposite IG domestic Airport, New
                 Delhi-110037
               </p>
-            </div>
-
-            <div className="flex space-x-4  mx-auto rounded-full  mt-4">
-              <FaFacebook className="text-blue-500 bg-white w-fit rounded-full text-2xl" />
-              <FaInstagram className="text-pink-500 bg-white w-fit rounded-full text-2xl" />
-              <FaWhatsapp className="text-green-600 bg-white w-fit rounded-full text-2xl" />
-              <FaYoutube className="text-red-500 bg-white w-fit rounded-full text-2xl" />
+              <div className="flex space-x-4  mx-auto rounded-full  mt-4">
+                <FaFacebook className="text-blue-500  w-fit hover:scale-110 transition-all cursor-pointer  text-2xl" />
+                <FaInstagram className="text-pink-500  w-fit hover:scale-110 transition-all cursor-pointer  text-2xl" />
+                <FaWhatsapp className="text-green-600  w-fit hover:scale-110 transition-all cursor-pointer  text-2xl" />
+                <FaYoutube className="text-red-500  w-fit hover:scale-110 transition-all cursor-pointer  text-2xl" />
+              </div>
             </div>
           </div>
 
