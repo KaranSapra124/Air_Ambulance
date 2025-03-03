ddce1e15560f6de60bc7fedba5f9dcdaaceedf2a
KaranSapra124 <sapra7112@gmail.com>
2025-03-03 22:47:14 +0530

    Responsive started

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -4,22 +4,35 @@ import ScrollAnimation from "react-animate-on-scroll";
 const Hero = () => {
   return (
     <>
-      <ScrollAnimation animateIn="backInRight" animateOnce duration={1.5} initiallyVisible>
-        <div className="relative h-full">
-          <div className="absolute bg-black/50 h-full w-full"></div>
-          <img src="./Home/Hero-bg.png" alt="" />
-          <div className="absolute inset-0 flex flex-col gap-2 justify-center items-center">
-            <h1 className="uppercase  text-5xl font-extrabold text-white text-center">
+      <ScrollAnimation
+        animateIn="backInRight"
+        animateOnce
+        duration={1.5}
+        initiallyVisible
+      >
+        <div className="relative lg:h-screen h-full w-full">
+          {/* Overlay */}
+          <div className="absolute  inset-0 bg-black/50"></div>
+
+          {/* Background Image */}
+          <img
+            src="./Home/Hero-bg.png"
+            alt=""
+            className="lg:w-full lg:h-full object-cover"
+          />
+
+          {/* Content */}
+          <div className="absolute inset-0 flex flex-col lg:gap-4 gap-1 justify-center items-center text-center px-4">
+            <h1 className="uppercase text-white font-extrabold lg:text-3xl text-sm md:text-5xl">
               your health is our priority
             </h1>
-            <div className="flex flex-col items-center justify-center  gap-2">
-              <button className="bg-white text-black rounded-full px-4 py-2 font-semibold">
-                download brochure
-              </button>
-              {/* <p className="text-white uppercase font-bold">see our services</p> */}
-            </div>
+            <button className="bg-white text-black rounded-full lg:px-6 lg:py-3 p-0.5 font-semibold lg:text-sm text-xs md:text-base">
+              download brochure
+            </button>
           </div>
-          <div className="h-1 w-8 bg-white shadow shadow-black mx-auto rotate-90"></div>
+
+          {/* Scroll Indicator */}
+          <div className="h-1 lg:w-8 w-4 bg-white shadow shadow-black mx-auto rotate-90 lg:mt-0 -mt-1"></div>
         </div>
       </ScrollAnimation>
     </>
