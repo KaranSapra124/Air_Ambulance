6affce504a5abb00b1139fc3d495f0f5bb9366c6
KaranSapra124 <sapra7112@gmail.com>
2025-03-03 22:39:28 +0530

    Responsive started

--- src/Components/Global/SubNavbar.jsx
+++ src/Components/Global/SubNavbar.jsx
@@ -4,15 +4,20 @@ import { FaMagnifyingGlass } from "react-icons/fa6";
 
 const SubNavbar = () => {
   return (
-    <>
-      <div className="flex bg-[#FEFFDE] justify-between px-4">
-        <img className="w-20" src="./Home/Logo.png" alt="" />
-        <div className="flex px-10  relative items-center ">
-          <input className="border rounded-full px-2 " placeholder="Search.." type="text" />
-          <FaMagnifyingGlass className="bg-black absolute left-[13.5rem] text-white p-1 text-2xl rounded-full"/>
-        </div>
+    <div className="flex items-center justify-between bg-[#FEFFDE] px-4 py-3 md:px-8">
+      {/* Logo */}
+      <img className="w-8 lg:w-20" src="./Home/Logo.png" alt="Logo" />
+
+      {/* Search Box */}
+      <div className="relative flex items-center w-full max-w-xs md:max-w-md">
+        <input
+          className="w-fit ml-auto border rounded-full lg:px-3 px-2 lg:py-2 py-1 text-xs lg:text-sm md:text-base outline-none"
+          placeholder="Search.."
+          type="text"
+        />
+        <FaMagnifyingGlass className="absolute right-3 text-gray-600 text-xs md:text-xl cursor-pointer" />
       </div>
-    </>
+    </div>
   );
 };
 
