1a90ae09b7ea7ddde15a680699b5a8f4baee812a
KaranSapra124 <sapra7112@gmail.com>
2025-03-03 23:38:59 +0530

    Responsive started

--- src/Components/Global/Footer.jsx
+++ src/Components/Global/Footer.jsx
@@ -4,11 +4,11 @@ import { FaFacebook, FaInstagram, FaWhatsapp, FaYoutube } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className="bg-[#d1361b] text-white ">
-      <div className="container p-6 mx-auto flex justify-between">
+      <div className="container lg:p-6 p-3 mx-auto flex lg:flex-row flex-col justify-between">
         {/* Quick Links Section */}
         <div className="flex flex-col">
-          <h5 className="text-lg font-bold mb-2">Quick Links</h5>
-          <ul>
+          <h5 className="lg:text-lg text-md font-bold mb-2">Quick Links</h5>
+          <ul className="text-xs lg:text-sm">
             <li>FAQ</li>
             <li>Our Pricing</li>
             <li>Terms & Conditions</li>
@@ -20,28 +20,28 @@ const Footer = () => {
         </div>
 
         {/* Connect with us Section */}
-        <div className="max-w-screen-sm text-center">
-          <h5 className="text-lg font-bold mb-2">Connect with us</h5>
-          <div className="flex items-center">
-            <p className="font-semibold">Phone No: 9990499116, 9990075511</p>
-            <div className="flex space-x-4  mx-auto rounded-full  mt-4">
-              <FaFacebook className="text-blue-500 bg-white w-fit rounded-full text-2xl" />
-              <FaInstagram className="text-pink-500 bg-white w-fit rounded-full text-2xl" />
-              <FaWhatsapp className="text-green-600 bg-white w-fit rounded-full text-2xl" />
-              <FaYoutube className="text-red-500 bg-white w-fit rounded-full text-2xl" />
+        <div className="max-w-screen-sm lg:text-center text-left">
+          <h5 className="text-md lg:text-lg font-bold mb-2 lg:my-0 my-2">Connect with us</h5>
+          <div className="flex lg:flex-row flex-col items-start   lg:items-center">
+            <p className="lg:text-sm text-xs font-semibold">Phone No: 9990499116, 9990075511</p>
+            <div className="flex space-x-4 mx-0 lg:mx-auto rounded-full mt-2  lg:mt-4">
+              <FaFacebook className="text-blue-500 bg-white w-fit rounded-full text-sm lg:text-2xl" />
+              <FaInstagram className="text-pink-500 bg-white w-fit rounded-full text-sm lg:text-2xl" />
+              <FaWhatsapp className="text-green-600 bg-white w-fit rounded-full text-sm lg:text-2xl" />
+              <FaYoutube className="text-red-500 bg-white w-fit rounded-full text-sm lg:text-2xl" />
             </div>
           </div>
-          <div className="flex items-center">
+          <div className="flex lg:flex-row flex-col items-center">
             <div>
-              <h6 className="font-bold mt-4">Main Office:</h6>
-              <p>
+              <h6 className="font-bold mt-2 lg:text-lg text-sm lg:mt-4">Main Office:</h6>
+              <p className="lg:text-sm text-xs">
                 H. NO. 652, Ground floor, Front portion SHAHBAD Mohammadpur, New
                 Delhi - 110061
               </p>
             </div>
             <div>
-              <h6 className="font-bold mt-4">BRANCH ADDRESS:</h6>
-              <p>
+              <h6 className="font-bold mt-2 lg:text-lg text-sm lg:mt-4">BRANCH ADDRESS:</h6>
+              <p className="lg:text-sm text-xs">
                 Ground floor, Meharam nagar, Opposite IG domestic Airport, New
                 Delhi - 110037
               </p>
@@ -51,8 +51,8 @@ const Footer = () => {
       </div>
 
       {/* Copyright Section */}
-      <div className="text-center bg-[#FE817180]  mt-4">
-        <p className="py-2 text-gray-900 font-semibold">
+      <div className="text-center bg-[#FE817180]  lg:mt-4 mt-2">
+        <p className="py-2 lg:text-sm text-xs text-gray-900 font-semibold">
           Copyright © All Rights Reserved. DriveLife Health Care & Human
           Resource
         </p>
