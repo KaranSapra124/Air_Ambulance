9944c6126f49dcea059499bf687d70fbbd3f4acb
karansapra124 <sapra7112@gmail.com>
2025-03-04 17:58:26 +0530

    Ambulance Page Started

--- src/Components/DropDown/Air_Ambulance.jsx
+++ src/Components/DropDown/Air_Ambulance.jsx
@@ -15,9 +15,9 @@ const Air_Ambulance = () => {
       />
       <GlobalContentContainer
         title={"Our ideology and approach of Air Ambulance Services:"}
-        content={
-          "Ideology: Sometimes, some situations demand you to hire affordable air ambulance Services. Our Air ambulances are specially designed aircraft meant for relocating patients from one destination to another facility while ensuring continuous medical monitoring along the way to ensure safety.Our approach: When the time runs out and ground ambulance support system threatens the safety of the patient our air ambulance services come into the scene. When the patient is in a remote place, our service also helps the patient in getting the right care"
-        }
+        content={[
+          "Ideology: Sometimes, some situations demand you to hire affordable air ambulance Services. Our Air ambulances are specially designed aircraft meant for relocating patients from one destination to another facility while ensuring continuous medical monitoring along the way to ensure safety.Our approach: When the time runs out and ground ambulance support system threatens the safety of the patient our air ambulance services come into the scene. When the patient is in a remote place, our service also helps the patient in getting the right care",
+        ]}
         image={
           "https://s3-alpha-sig.figma.com/img/6afa/bc58/4968e18e6f1c7acedd06b2f3a8a6e2d3?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=pyONVYhvfohQkKWdn~NiFixfFtxmaFpgdx5Bfa02ZV1K~0vQwIN-b5xaVEWJkDx3EsWuFoQtfzgcMsJ9Vujti4XhSSCoM2CZaz~5eGIGQTGsj9Sk3V~BN3zl-PS2mP8NpSJVEsxycqxeork6nv5Ct7~zzxSyUePbFfbRzPJCqtnsbdEXW51EdIJZjMiOJ5ln9opotOY8Urk2QwKYlkGj7MX8fk-kef6qu18R06VX81Bg6H7R3qQSq4l9j1CI34w-3cI4LQSKYBKjT-4z6CPGNZUqiaooL~EDo-~SyKHvCNXKuO5vqFIEjVmtZfQlrlPwGDVzqj3e0UzwTfHfuaKGAg__"
         }
--- src/Components/Global/GlobalContentContainer.jsx
+++ src/Components/Global/GlobalContentContainer.jsx
@@ -7,7 +7,13 @@ const GlobalContentContainer = ({ title, content, image }) => {
         <h1 className="text-primary-color my-2 text-center lg:text-2xl text-sm font-bold">
           {title}
         </h1>
-        <p className="text-primary-color my-2 font-semibold text-center lg:text-sm text-xs ">{content}</p>
+        {content?.map((elem, index) => {
+          return (
+            <p className="text-primary-color my-2 font-semibold text-center lg:text-sm text-xs ">
+              {elem}
+            </p>
+          );
+        })}
         <img src={image} alt="" srcset="" />
       </div>
     </Container>
