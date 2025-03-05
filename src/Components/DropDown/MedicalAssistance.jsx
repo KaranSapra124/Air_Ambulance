30b6f7290d20579069d6806bb1f94afd5b0a902a
KaranSapra124 <sapra7112@gmail.com>
2025-03-06 00:25:05 +0530

    Added assistance

--- src/Components/DropDown/MedicalAssistance.jsx
+++ src/Components/DropDown/MedicalAssistance.jsx
@@ -1,13 +1,37 @@
 import React from "react";
 import GlobalHero from "../Global/GlobalHero";
+import GlobalServiceContent from "../Global/GlobalServiceContent";
+import GlobalContentContainer from "../Global/GlobalContentContainer";
 
 const MedicalAssistance = () => {
   return (
     <>
-      <GlobalHero title={"Standby Medical Assistance"}
+      <GlobalHero
+        title={"Standby Medical Assistance"}
         image={
           "https://s3-alpha-sig.figma.com/img/3a4e/9c77/8cfcdbfe85d0692cf6c15f7061631736?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=mEg9ad0ionyAQV210EAuzCqZk-SDjBgWH~ObuNezOgJ3UXE9SXZUNl9-d1iWkSwX48tAGHbF8~7u9HP6SydFHkD~hzhSH9ufqdSnMAxbd6uq6fcBKB4uRBPXTlOS4N67UFMX7tAVY3q95MFEGM~u0J9RH7PJRPsm2Tbsb0DJWUdmDqZJcj1E91arJODvpKNwWr7uS2b3UscGWC-Yyjb48TzahWZte2tYHzsRwi6l6SIZ6hQPgOgQrHBxpERmeD9NIQRuR7tB~JDeUid6fP~fLqiDOiurA4JS81za6HILc-ja~47yXmIpUT7V2cv4SPCqVlzU7kqhfC0PZM5MMXAb5g__"
         }
+      />
+      <GlobalServiceContent
+        title={"Outsource Quality Medical Services from Certified Staff"}
+        content={[
+          "The DHHR is a popular medical emergency service provider that is available at your service 24 x 7. We have an in-house trained and certified staff for all types of medical care. We can guarantee you a premium level of care and no matter, whether you are in need of a full medical team or a single attendee, we offer you the best.",
+        ]}
+      />
+      <GlobalContentContainer
+        title={"On-site medical care service by experts"}
+        content={[
+          "You can call as the best stand by medical services provider because we include a different number of expert personnel which includes medical technicians like nurses, emergency responders, qualified doctors, and others.",
+          "Our onsite medical services also include servicing medical care in outdoor and indoor events, during religious meet, festivals and more. You can outsource or medical services 24 x 7. All our ambulances are best equipped with airway management, oxygen machines, trauma medication, suction, and other tools required for emergency medical care.",
+        ]}
+      />
+      <GlobalContentContainer
+        title={"Medical care even in remote areas"}
+        content={[
+          "Our expert staff members include nurses, physicians, assistants, and other members to form a qualified and certified team. Basically, our sports medical services can be offered at any place. This falls in the category of on-site medical service and it is commonly found in the remote areas. DHHR, promises high-end service from experienced and qualified medical staff. You can call us as the most sought service provider of medical care and treatment. We can assure you of quality rated services, which you will never find anywhere else.",
+        ]
+    }
+    image={'https://s3-alpha-sig.figma.com/img/35e1/5a2c/448b94d11234b0b649c0b579a6500c4a?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=cXeCdpeVaaYqccGyhrhZ3KAv3olIo4GRM-D8E472JLDfz-PyGZmqTYiyrRjYe-WdGEW-Hu3uD~UItkQHuvI~vKeJrvkfTX9FUSGoFtABQT5ppWYMNNUH-20IAxH~uY-10hJTAsnoAa8CWMZ4Of6MjKPA~gDjLefZ71gVUb-2dTpAktmeB4oBadjZWRmTlb6qIcpB1tE9FirqxWlSPdSyGvO2X49KAgNcnMPUBooDMZozRQobCdgj5aBSKliIwAH9uTSDOLZcrN2w9kv5R6H0E~wuMSjLIWP6Lib80TSWoD2DeReZvgzTvM9kSftF65Mq56z5Ass43U3uEL6wI9mGbw__'}
       />
     </>
   );
