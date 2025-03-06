dbf349e3b1fb347e50abab961bb33680ec9d90d7
karansapra124 <sapra7112@gmail.com>
2025-03-06 15:21:12 +0530

    Added Stretcher

--- src/Components/DropDown/Stretcher.jsx
+++ src/Components/DropDown/Stretcher.jsx
@@ -28,6 +28,16 @@ const Stretcher = () => {
           "Cost friendly option: If you are hiring an emergency aircraft then you should be ready to pay a hefty amount but if you choose a commercial stretcher then you will be able to pay less for the same kind of service because it would eliminate paperwork.",
         ]}
       />
+      <GlobalServiceContent title={"What are the Features of commercial Stretcher"} content={['Medical Assistance in the air: We offer a medical team that would also all the clients and patients in the air. Our team ensures that the patients do not have issues in the air. We have highly trained staffs and medical escorts to','International Experience: if you want to travel to international destinations then you should consider our service because we are the best on that front, from dealing with visa to getting you the right commercial flight, we can do everything','Robust support system: The first thing that is required to make medical transportation seamless is a great support system; we have a strong network that offers ground support and other desired service for the transition.','Consultation and assurance: we are well aware of the fact that some clients find it difficult fund the right kind of the medical facility, but with us this problem is irrelevant because we have a team that offers assurance/','From finding the right kind of the doctors to finding the treatment package, they can do everything. This approach makes sure the best and most preferred organization in the market for air ambulance service.','Quick solution: We make sure that we arrange our ambulance as quickly as possible, but you need to book in advance because we are dependent on the airlines, we make sure that we arrange everything within the given time frame so that we can give service quickly.','In addition, we ensure that we have a tram that deals with patients other needs like offering a life support system and medical supplies, we also ensure that your needs are completely taken care of.']} />
+      <GlobalContentContainer
+        title={"How do we arrange the Stretcher In Commercial Flight ?"}
+        content={[
+          "We arrange the stretcher in the commercial on the special demand of customers. To book stretcher commercial flight; you will have to inform us before 24  hours take of the flight. So that we could arrange all the things properly.",
+          "We remove 9 seats from flight to setup stretcher in flight. It is done by a medical expert team who know well to setup it. So don’t worry about anything.",
+          "DHHR one of the best stretchers in commercial services provider at affordable price.",
+        ]}
+      />
+      <GlobalServiceContent title={'Contact us today to hire commercial flight with stretcher facilities.'} content={['If you are looking for commercial airline repatriation then you should consider our service. We ensure that we offer cist friendly ad quick solution, your search should ideal to stop here because we are the best, call us if you have a need for commercial ambulance service ad we would love to act.']}/>
     </>
   );
 };
