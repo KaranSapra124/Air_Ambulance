69de0aa87225df93f05584d6f34c5589a18fa18f
karansapra124 <sapra7112@gmail.com>
2025-03-06 15:39:45 +0530

    Started IGI

--- src/Components/DropDown/Ambulance_In_IGI.jsx
+++ src/Components/DropDown/Ambulance_In_IGI.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import GlobalHero from "../Global/GlobalHero";
+import GlobalServiceContent from "../Global/GlobalServiceContent";
+import GlobalContentContainer from "../Global/GlobalContentContainer";
 
 const Ambulance_In_IGI = () => {
   return (
@@ -9,6 +11,25 @@ const Ambulance_In_IGI = () => {
           "https://s3-alpha-sig.figma.com/img/b366/4967/b63291de07701ed4605196f1c3fc4e9b?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=pcnEdOpK~WcEw08N0n7-VsH3xVgmSty9HngyM7JUWnkmEDdsLz6IhmmNiTFvIww2yThpQTudPJL~~ZZnpVzZmtJ6mEuTo~PMGVUkQBKEZQNKPBBDiJkwIXG9gpCxhmCOkaDujaTkXOmu591n9l0pT-y99W3mGT-glZq~UbB99xkXsDVywweQuHDHZQ6t1R~UceC0YwDhXY2tIcIkLdpNimqUoBhrJnlmEBS~T7VaZN8hd2LG7N2tYK37s6ZvT8NW0-tuAIxd8fL1mKxC3Ia1DAaV8DCmv6f3eYkt4OiwPm7FimdD-72lp65NhGjt-hSF-Sl2H8MjT5Y9vZLmsDt5Sw__"
         }
         title={" Air Ambulance Services Near IGI Airport Delhi"}
+      />
+      <GlobalServiceContent
+        title={"Hire Best Air Ambulance Service at IGI Airport New Delhi"}
+        content={[
+          "👉 The Air Ambulance is one of the fastest expanding in the world in the Subcontinent. Operating services on both national and international flights on an emergency and non-emergency basis.",
+          "👉 It serves primarily in air, train, and Road ambulance situations in an emergency service. The Air Ambulance Service Near IGI Airport has a broader experience in patient transport across states or countries.",
+          "👉 The team knows their areas best. Trained aviators are the fundamental theme behind all ventures in the air ambulance. Use the intellectual capacity and brain intelligence for the well-being and effective transition of the patients.",
+        ]}
+      />
+      <GlobalContentContainer
+        title={"Book Air Ambulance at IGI Airport at Affordable Cost"}
+        content={[
+          "The DHHR Rescue team is comprised of doctors, paramedics, people whose experience in the healthcare area provides a high level of service and assistance.",
+          "The services range from clinical escorts to commercial aviation service, through chartered flights, to the evacuation of hospitalized patients in India and overseas.",
+          "There are three main aspects of an air ambulance",
+          "👉 A fixed-wing aircraft",
+          "👉 A qualified and experienced doctor",
+          "👉 The medical equipment",
+        ]}
       />
     </>
   );
