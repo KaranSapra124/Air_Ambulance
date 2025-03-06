2fd28ebc557e7595129e706a74fe68dad18f2578
karansapra124 <sapra7112@gmail.com>
2025-03-06 16:32:08 +0530

    Added Animations To Components

--- src/Components/Global/GlobalContentContainer.jsx
+++ src/Components/Global/GlobalContentContainer.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import Container from "../Global/Container";
+import ScrollAnimation from "react-animate-on-scroll";
 const GlobalContentContainer = ({ title, content, image }) => {
   return (
     <Container>
-      <div >
-        <h1 className="text-primary-color my-2 text-center lg:text-2xl text-sm font-bold">
-          {title}
-        </h1>
-        {content?.map((elem, index) => {
-          return (
-            <p className="text-primary-color my-4 font-semibold text-center lg:text-sm text-[0.6rem] ">
-              {elem}
-            </p>
-          );
-        })}
-        {image && <img src={image} className="mx-auto lg:w-full lg:h-96 object-cover" alt="" srcset="" />}
-      </div>
+      <ScrollAnimation animateIn="backInUp" animateOnce>
+        <div>
+          <h1 className="text-primary-color my-2 text-center lg:text-2xl text-sm font-bold">
+            {title}
+          </h1>
+          {content?.map((elem, index) => {
+            return (
+              <p className="text-primary-color my-4 font-semibold text-center lg:text-sm text-[0.6rem] ">
+                {elem}
+              </p>
+            );
+          })}
+          {image && (
+            <img
+              src={image}
+              className="mx-auto lg:w-full lg:h-96 object-cover"
+              alt=""
+              srcset=""
+            />
+          )}
+        </div>
+      </ScrollAnimation>
     </Container>
   );
 };
