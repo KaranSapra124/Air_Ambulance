3bc77edcf80b82c8e4bf30fa62e5fd5d4ee17dbb
karansapra124 <sapra7112@gmail.com>
2025-03-06 15:45:26 +0530

    Completed IGI

--- src/Components/DropDown/Ambulance_In_IGI.jsx
+++ src/Components/DropDown/Ambulance_In_IGI.jsx
@@ -30,7 +30,35 @@ const Ambulance_In_IGI = () => {
           "👉 A qualified and experienced doctor",
           "👉 The medical equipment",
         ]}
+        image={
+          "https://s3-alpha-sig.figma.com/img/147b/110c/ac687f5db62d973d297393e9d26564da?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=a1dAi2x630Zxg4x4I5D~dsC-r5CGVnMB~p4aTnQp5EUK5-wdHYbc9koQd6~az3Ydi-5bHiw17O8mrJYer3v~baNHP6k1T1fV4xNBeSVeKohwWlUGZY65MZDIcj7lQC-RDxXheZeoQZPdl6sr9DRYUu4Ez2UhO9Gu6U9d4rAuIKJRsaWHoSJK90i2tW05Bcoy3tyoIkGr9KrHxO2H2PY4s0dvlwSKYUy3LW84PhF1Acqd9oGfChhluIPtwnyiziFbfeLyduBfCxGyITj1hQ8Nifd873oIHPjM2nNMTasG63KacH9rMLkFm2B8H6be0vEvjwv18M0rME9eMeL5kycVFA__"
+        }
       />
+      <GlobalServiceContent
+        title={"What is an Air Ambulance Services at IGI?"}
+        content={[
+          "👉 Air Ambulance is the most fast, safe, and time-saving way to move patients who have become seriously ill from their source to the destination affordably and inexpensively.",
+          "👉 The air ambulance service at IGI Airport Delhi is fully equipped with ICU facilities to ensure that patients get safe and successful treatment during or before their destination is reached.",
+          "👉 Despite the patient’s condition and where it is supposed to be shifted,DHHR will do its utmost to move those patients to a safer location as soon as possible. Air Ambulance includes a hospital bed stretcher on board.",
+        ]}
+      />
+      <GlobalContentContainer
+        title={"Who is on board the aircraft at IGI Airport?"}
+        content={[
+          "To ensure that the patient on board is given appropriate medical treatment onboard each emergency flight, medical professionals, and paramedics depending on the case to case. Comprehensive education for emergency rescue, safety operations have been given to both doctors and paramedics.",
+          "DHHR squad comprises surgeons and paramedics with a long tradition of treating adverse medical conditions. It consists of a cardiologist, neurologist, researcher, orthopedic, specialist for emergency care, respectively.",
+        ]}
+      />
+      <div className="-mt-16">
+        <GlobalContentContainer
+          title={"How does this service work?"}
+          content={[
+            "After receiving the call and information about a patient’s medical condition for aircraft, the Air ambulance service at IGI Airport informs/alerts the medical team that reviews the patient’s medical history and draws up an Action Plan.",
+            "After Final approval by the family hospital, they will be at your city or a hospital in an hour to collect the patient then board the ICU aircraft to move the patient in or out of the country or state.",
+            "They also organize ground hospital facilities in many locations to move patients to airports from the hospital.",
+          ]}
+        />
+      </div>
     </>
   );
 };
