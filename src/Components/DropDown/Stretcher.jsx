dec247c8f709595f4c1604b81eae74ade83c25da
karansapra124 <sapra7112@gmail.com>
2025-03-06 15:16:25 +0530

    Added Stretcher

--- src/Components/DropDown/Stretcher.jsx
+++ src/Components/DropDown/Stretcher.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React from "react";
 import GlobalServiceContent from "../Global/GlobalServiceContent";
 import GlobalContentContainer from "../Global/GlobalContentContainer";
 import GlobalHero from "../Global/GlobalHero";
 const Stretcher = () => {
   return (
     <>
-    <GlobalHero image={'https://s3-alpha-sig.figma.com/img/0da6/5249/ef00df45b7d084c2ec3671e60dfd96b4?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=twvp2MPj9f6wCwZjSVOYo0mOTbJCnRatEt09DDvrCzmu5R2gLxH~5vSeXioUBkyOT~HM87y-MqQaPg-q2CB0COEIM1YVyoKEMKQK6K7HPePU0ySsxpZYUlJ5GQLxTiCUHRDtxMcWPoluei2WiYw707s21-GCcdKEOZqWZbBHGgyZujKMVT1BFo7Obtak5~6brzBefCrDTSq4yY913wK6lz0Lu2TdCPnZgS~pR5MW~8ciDgSd3Xc8Z-Lnuh-WWnp05LJq1R-sT0pcY6PTWUJCVHMNi3Dk7LUwCQYn9HLzmFu52ln0Qo4j0VK2nmPFhfksMYDF4ik7bp7ex0FNPMq2TQ__'} title={'Stretcher In Commercial Flights'}/>
+      <GlobalHero
+        image={
+          "https://s3-alpha-sig.figma.com/img/0da6/5249/ef00df45b7d084c2ec3671e60dfd96b4?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=twvp2MPj9f6wCwZjSVOYo0mOTbJCnRatEt09DDvrCzmu5R2gLxH~5vSeXioUBkyOT~HM87y-MqQaPg-q2CB0COEIM1YVyoKEMKQK6K7HPePU0ySsxpZYUlJ5GQLxTiCUHRDtxMcWPoluei2WiYw707s21-GCcdKEOZqWZbBHGgyZujKMVT1BFo7Obtak5~6brzBefCrDTSq4yY913wK6lz0Lu2TdCPnZgS~pR5MW~8ciDgSd3Xc8Z-Lnuh-WWnp05LJq1R-sT0pcY6PTWUJCVHMNi3Dk7LUwCQYn9HLzmFu52ln0Qo4j0VK2nmPFhfksMYDF4ik7bp7ex0FNPMq2TQ__"
+        }
+        title={"Stretcher In Commercial Flights"}
+      />
+      <GlobalServiceContent
+        title={"Commercial Airline Stretcher for Quick Care"}
+        content={[
+          "If you are looking for commercial airline stretcher service then you should consider us. Commercial airline stretcher facility is ideal for a variety of situations that include changing medical facilities where the patients do not want to spend more money on an emergency air ambulance.",
+        ]}
+      />
+      <GlobalContentContainer
+        image={
+          "https://s3-alpha-sig.figma.com/img/c7ba/ea5d/358f4c2fe580cfd4bb9676e92c08d22a?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=sS2TnE137CHyAnnxRoRz0ojuPalxiXMmOyd8BdqHWPmEHHPFamIutYMPEhRFt2PT~2tMw7Datlo9kL~8imXGRwcErVBwL-5Po~zw8uACp62o6TDKIVC-l5yfbv~d3ATm7oXOzQ2Kvjdq0YR6DYI2th1aSsjb~p4yGExcrxlCetzko-QBNr-iGTM-LB0UNwbjO2Uog3usij25hC9Agpokmi6JAIR0uG~kWfbGa-aE2yN5rAYBck2~zzxsrv~6unUn8tfY2hAgpmkkrIF-nzqg577AzdUBOCvAGJp0D5tVsCKEDwSRHplkCplZFCOB84eg8VVL2eXx6tCHwU9S4J-PQg__"
+        }
+        title={"Why should you fly with us?"}
+        content={[
+          "Superb commercial stretcher:  We as the best commercial airline stretcher offer bed-to-bed service in our commercial stretchers; you will be accompanied by a team of medical professionals that includes nurses, paramedics and medical escorts for secure and safe transportation.",
+          "Comprehensive commercial Airline Stretcher Service: We offer commercial service. From business class stretcher service to the economic class stretcher service, we offer a comprehensive solution. We also offer international air commercial stretcher transport service to the clients who want to visit medical facilities located elsewhere in the world.",
+          "Cost friendly option: If you are hiring an emergency aircraft then you should be ready to pay a hefty amount but if you choose a commercial stretcher then you will be able to pay less for the same kind of service because it would eliminate paperwork.",
+        ]}
+      />
     </>
-  )
-}
+  );
+};
 
-export default Stretcher
+export default Stretcher;
