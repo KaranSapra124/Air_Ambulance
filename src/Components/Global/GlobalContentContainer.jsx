43601db0ce96d7b1da4b4c815beb956bbe9c90a1
karansapra124 <sapra7112@gmail.com>
2025-03-06 18:30:10 +0530

    done

--- src/Components/Global/GlobalContentContainer.jsx
+++ src/Components/Global/GlobalContentContainer.jsx
@@ -9,6 +9,8 @@ const GlobalContentContainer = ({ title, content, image }) => {
           <h1 className="text-primary-color my-2 text-center lg:text-2xl text-[0.7rem] font-bold">
             {title}
           </h1>
+          <div className="w-10 h-0.5 lg:h-1  bg-primary-color rounded-full mx-auto my-1"></div>
+
           {content?.map((elem, index) => {
             return (
               <p className="text-primary-color lg:my-4 my-1 font-semibold text-center lg:text-sm text-[0.6rem] ">
