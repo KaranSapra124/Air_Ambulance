e679260832ff3b2b700e88dd2c571c614ac254da
karansapra124 <sapra7112@gmail.com>
2025-03-07 12:57:35 +0530

    Added GlobalBlogs

--- src/Components/Blogs/BlogsData.jsx
+++ src/Components/Blogs/BlogsData.jsx
@@ -14,6 +14,22 @@ const BlogsData = ({ isHeading }) => {
         "https://s3-alpha-sig.figma.com/img/38ed/fc35/b0609b77a18a0bbf3e9262d849f061b3?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=gIbJEURWRwmOLVl7S9HeLRSjAEJ3EGm-A-OmYeGNEN~tjH2Zk2QdFNFZvOIu2jEs2KtOMza1tv8EPUSxlotqoDfoUZM3JTdKypKU-mqGz6hH8bhl-LzwVy~AoDg0i5STsG0kpalgRzKyLmrAXTMKeC8emb9WFzm~K1KwREJjIjy~Frh1diAD4GbKWThpA1liKAVp3blcQPrZYqSma2XgtshE0ryk8mgbBtr8zmuW59whPuhiafm2s0SOQdzgmUzFYBfjSPrFPwNzE33l9R5JIZW7jGx96QGxjmV9CikldjmkYn4n2iPl8O1RKMBSKGF8G1r7~tVrhM4j5x8Wk7ViJA__",
       date: "May 30, 2021",
       desc: "Confronted with medical emergencies, you are likely to get agile and stressed because those situations are not the easiest of the things to handle and people even with nerves of steels get broken during those medical emergency situations, you definitely need a good ambulance service provider at that time.",
+      image2:
+        "https://s3-alpha-sig.figma.com/img/b5a9/566b/98aba35d038d396d0d23f83a17927ee2?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=NWLhgKRPBAt~OMBLqW~WWlK4KvE6H8Ou6zPFckgWN-pvFyN~bQulokzLzAC9KArdejzZkwmjqzGR5OiyYjnGjnIJexHjDsI9AkstkZgiT9xKugI0C0pgXmCmcMKePbDPK8vo5xfPLFKzhkGZRfRiR4IO8LjM1US18DBO8pyeBpqAx6hZfC9M4Vpgq9J57uOAXoNPhcB4p~1Xw4snvc8pmPgV3gnk~sjePHtZ6jFHoKECHrwWefrCvTL~HbaKZinEnoV9RE-~Ag9iHGZYf7svRKZmMptb-7Zhpg91v5sEmO~yfWeX8mqFKOOacoc5bJ6SYYQa2zWYTXRREgfFprUpJw__",
+      arr: [
+        {
+          title: "The need for ambulance services:",
+          desc: "The first thing that you have to find out is the ways of transporting the patient to the right medical facility and here ambulance can do the job, moreover, they will have all the LS kits and paramedics to transport the patient safely to the medical facility.You can get Train Ambulance Services from companies like Lakshya Rescue and transport patients to different cities across the nation, train ambulance come in handy when you have patients who need constant medical attention during the transition process.",
+        },
+        {
+          title: "Choosing the right ambulance type:",
+          desc: "You have road ambulance that can help you in transporting patients to the medical facilities in your locality or in your city, and then you have a train ambulance that would help you in transporting you in a different city and then you have Air Ambulance Services that can make quick transportation both domestically and internationally.You have to choose the right type of ambulance and that would depend upon where you want to go for medical transportation how quickly you need to reach the medical facility. In addition, the cost of the different ambulance service would differ and vary, air ambulance might cost you more as that is fats, and train ambulance might take a little time.You have to understand each type of ambulance operational modalities and the cist factors before choosing the ambulance service for your needs.",
+        },
+        {
+          title: "Find a good ambulance provider:",
+          desc: "You have to make sure that you are getting the right ambulance service provider, which would mean that you have to look for one and you can get the best ambulance orders on the web or through references, you should make sure that you verify how good they are and that you can do by having a  look at the customer testimonials.However, there are many other factors that you must look at before picking a service provider for your ambulance and medical emergency needs and here are those points that you must consider.👉 You have to find an ambulance service provider that has smart dispatching systems, they should be able to send the ambulance and arrange them for you as quickly as possible.👉 You have to find out how good they are in terms of transportation and how good their network is, a good service provider will have strong network through which you can get better medical facility quickly.👉 They should and must have good tools, medical devices, and smart paramedics because the thing would aid to your safety of transportation.If you have been looking for the Air Ambulance Cost, then you might have to look at the listed service offered by DHHR  because they are specialist and have been doing a great job for a long time in helping patients, talk to them now.",
+        },
+      ],
     },
     {
       title:
@@ -65,7 +81,10 @@ const BlogsData = ({ isHeading }) => {
           {blogsData?.map((elem, index) => {
             return (
               <>
-                <div className="lg:w-72 lg:my-2  w-64  mx-auto shadow-lg shadow-black relative h-96  rounded-4xl hover:scale-105 transition-all cursor-pointer bg-primary-color">
+                <div
+                  onClick={() => handleOpenBlog(elem)}
+                  className="lg:w-72 lg:my-2  w-64  mx-auto shadow-lg shadow-black relative h-96  rounded-4xl hover:scale-105 transition-all cursor-pointer bg-primary-color"
+                >
                   <img
                     className="lg:w-full "
                     src={elem?.image}
