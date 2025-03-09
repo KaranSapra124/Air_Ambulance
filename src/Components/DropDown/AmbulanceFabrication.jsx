43f04d33286d85dc23f1fafebb9137d99785f768
karansapra124 <sapra7112@gmail.com>
2025-03-09 18:06:07 +0530

    More pages started

--- src/Components/DropDown/AmbulanceFabrication.jsx
+++ src/Components/DropDown/AmbulanceFabrication.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import GlobalHero from "../Global/GlobalHero";
+import GlobalServiceContent from "../Global/GlobalServiceContent";
+import GlobalContentContainer from "../Global/GlobalContentContainer";
 
 const AmbulanceFabrication = () => {
   return (
@@ -11,6 +13,28 @@ const AmbulanceFabrication = () => {
         title={
           "Professional Ambulance Fabrication Services for Quick and Reliable Medical Transport"
         }
+      />
+      <GlobalServiceContent
+        title={"We Promote Quick & Efficient Medical Transportation"}
+        content={[
+          "If you are looking for high-quality Ambulance Fabrication Services, you have come to the right place. We specialize in designing and fabricating customized ambulances to meet your specific needs, ensuring quick and safe patient transportation during emergencies.",
+        ]}
+      />
+      <GlobalContentContainer
+        title={"Why Choose Our Ambulance Fabrication Service?"}
+        content={[
+          "👉 Superior Ambulance Fabrication: We offer top-notch ambulance fabrication services with high-quality materials and advanced medical equipment. Our fabrication ensures that the ambulance is fully equipped to handle any medical emergency. We build ambulances with the latest technology and customized interiors for patient safety and comfort.",
+          "👉 Affordable Fabrication Solutions: Our ambulance fabrication service is cost-effective, ensuring you get the best quality without exceeding your budget. We understand the importance of medical transport and ensure that our services remain affordable and reliable.",
+        ]}
+        image={
+          "https://plus.unsplash.com/premium_photo-1664299503603-237d266dc7cb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fGFtYnVsYW5jZSUyMGZhYnJpY2F0aW9ufGVufDB8fDB8fHww"
+        }
+      />
+      <GlobalServiceContent
+        title={"Book Our Ambulance Fabrication Service Today"}
+        content={[
+          "If you are looking for a reliable and affordable Ambulance Fabrication Service, we are your ideal solution. We guarantee quick fabrication, high-quality medical equipment installation, and after-sale support to ensure smooth and safe patient transportation. ✅ Contact us today to get your customized ambulance fabricated. We assure you the best service at an affordable price.",
+        ]}
       />
     </>
   );
