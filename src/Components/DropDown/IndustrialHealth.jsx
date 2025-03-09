37133e4af0b21458ec5aed9414699e42665ba6f3
karansapra124 <sapra7112@gmail.com>
2025-03-09 17:34:41 +0530

    More pages started

--- src/Components/DropDown/IndustrialHealth.jsx
+++ src/Components/DropDown/IndustrialHealth.jsx
@@ -34,8 +34,27 @@ const IndustrialHealth = () => {
             "👉 Emergency Response for major injuries, ensuring prompt action and transportation if required.",
             "👉 Health Monitoring for employees exposed to hazardous chemicals or high-risk tasks.",
           ]}
+          image={
+            "https://plus.unsplash.com/premium_photo-1661377235003-ba4de23dff90?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZW1wbG95ZWUlMjBoZWFsdGh8ZW58MHx8MHx8fDA%3D"
+          }
         />
       </div>
+      <GlobalServiceContent
+        title={"Features of Our Medical Rooms"}
+        content={[
+          "👉 Medical Assistance on Site: We provide a team of qualified healthcare professionals, including paramedics and trained medical staff, to ensure that your employees receive quick medical attention without leaving the premises. Our round-the-clock service helps in minimizing health risk.",
+          "👉 International Standards of Care: We follow international industrial health standards to ensure that the medical room is equipped with the latest facilities and advanced first aid equipment. Our services comply with the Factories Act, 1948, ensuring a safe working environment.",
+          "👉 Robust Support System: We have a strong support network to ensure that if any critical situation arises, we can quickly arrange an ambulance or connect with nearby hospitals. Our team also coordinates with your existing health policies to make the treatment seamless.",
+          "👉 Health Consultation and Guidance: We understand that some workers may face difficulty in finding the right medical guidance. Our team provides health consultation, ensuring that they receive proper guidance regarding medical facilities, treatments, and health improvement plans.",
+        ]}
+      />
+      <GlobalContentContainer
+        title={"Book Our Industrial Health (Medical Room) Service Today"}
+        content={[
+          "If you are looking for a reliable and affordable Industrial Health (Medical Room) service, we are your ideal solution. We ensure quick response, expert medical care, and seamless support to keep your employees safe and healthy.",
+          "✅ Contact us today to set up a medical room in your industry. We guarantee you the best service at an affordable cost.",
+        ]}
+      />
     </>
   );
 };
