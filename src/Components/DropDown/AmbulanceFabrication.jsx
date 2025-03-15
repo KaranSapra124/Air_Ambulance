7c202c98a19902918851d14ec5528dba3114b757
karansapra124 <sapra7112@gmail.com>
2025-03-15 17:31:17 +0530

    Added changes

--- src/Components/DropDown/AmbulanceFabrication.jsx
+++ src/Components/DropDown/AmbulanceFabrication.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import GlobalHero from "../Global/GlobalHero";
 import GlobalServiceContent from "../Global/GlobalServiceContent";
 import GlobalContentContainer from "../Global/GlobalContentContainer";
 import GlobalPointsContainer from "../Global/GlobalPointsContainer";
 import Container from "../Global/Container";
+import { Carousel } from "antd";
 
 const AmbulanceFabrication = () => {
+  const [animateKey, setAnimateKey] = useState(0);
+
+  const ambulancePics = [
+    "AmbulancePics/20191201_124721.jpg",
+    "AmbulancePics/20191201_124738.jpg",
+    "AmbulancePics/20191201_182911.jpg",
+    "AmbulancePics/20200421_131440.jpg",
+    "AmbulancePics/20200423_123843.jpg",
+    "AmbulancePics/20200718_185721.jpg",
+    "AmbulancePics/20200718_185736.jpg",
+    "AmbulancePics/20200718_185915.jpg",
+    "AmbulancePics/20200718_190402.jpg",
+    "AmbulancePics/20200810_160621.jpg",
+    "AmbulancePics/20200810_160649.jpg",
+    "AmbulancePics/20200816_162153.jpg",
+    "AmbulancePics/20200816_162157.jpg",
+    "AmbulancePics/20200816_162207.jpg",
+    "AmbulancePics/20200816_162211.jpg",
+    "AmbulancePics/20200911_180623.jpg",
+    "AmbulancePics/20200911_184500.jpg",
+  ];
+
   return (
     <>
       <GlobalHero
@@ -68,15 +91,53 @@ const AmbulanceFabrication = () => {
           title={"Why Choose Us ?"}
           para={""}
           points={[
-            "High-Quality Fabrication: Built with precision and top-grade materials"," Customization: Tailored solutions to fit your specific needs"," Safety Compliance: Meets all regulatory and industry standards"," Durability & Reliability: Designed for long-term performance"," Affordable & Timely Delivery: Cost-effective solutions with fast turner",
+            "High-Quality Fabrication: Built with precision and top-grade materials",
+            " Customization: Tailored solutions to fit your specific needs",
+            " Safety Compliance: Meets all regulatory and industry standards",
+            " Durability & Reliability: Designed for long-term performance",
+            " Affordable & Timely Delivery: Cost-effective solutions with fast turner",
           ]}
         />
       </Container>
-      <GlobalContentContainer
+      {/* <GlobalContentContainer
         image={
           "https://plus.unsplash.com/premium_photo-1664299503603-237d266dc7cb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fGFtYnVsYW5jZSUyMGZhYnJpY2F0aW9ufGVufDB8fDB8fHww"
         }
-      />
+      /> */}
+      <div className="relative">
+        <Container>
+          <Carousel
+            autoplay
+            autoplaySpeed={3500}
+            dots={false}
+            beforeChange={() => setAnimateKey((prev) => prev + 1)} // Update animation key
+          >
+            {ambulancePics?.map((pic, index) => {
+              return (
+                <div key={index} className="relative">
+                  {/* Background Image */}
+                  <img
+                    className="lg:max-h-[30rem] h-52 w-full"
+                    src={pic}
+                    alt={`Slide ${index}`}
+                  />
+
+                  {/* Overlay */}
+                  <div className="absolute inset-0 bg-red-500/50 bg-opacity-50 flex items-center justify-center">
+                    <h2
+                      key={animateKey} // Key update forces re-animation
+                      className="text-white ambulance-animation text-sm lg:text-3xl font-bold text-center"
+                    >
+                      You Think It, We Do It
+                    </h2>
+                  </div>
+                </div>
+              );
+            })}
+          </Carousel>
+        </Container>
+      </div>
+
       <GlobalServiceContent
         title={"Book Our Ambulance Fabrication Service Today"}
         content={[
